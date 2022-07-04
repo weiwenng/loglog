8cba29dc5ae63ade0038504c99005120d3f9b50e
weiwen <weiwen.97@hotmail.com>
2022-07-04 16:13:03 +0800

    update

---
+++ loggers/warehouse/views.py
@@ -0,0 +1,15 @@
+
+from .models import Todo
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import TodoSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    ## The Main Query for the index route
+    queryset = Todo.objects.all()
+    # The serializer class for serializing output
+    serializer_class = TodoSerializer
+    # optional permission class set permission level
+    permission_classes = [permissions.AllowAny] #Coule be [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAuthenticated]
