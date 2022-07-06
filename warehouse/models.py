fc032d51fce11900f85934b3cd2b9131dd05dd19
weiwen <weiwen.97@hotmail.com>
2022-07-06 15:46:46 +0800

    update foodlist choices

--- warehouse/models.py
+++ warehouse/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 class Todo(models.Model):
@@ -11,15 +12,16 @@ class Menu(models.Model):
     price = models.IntegerField()
 
 class FoodList(models.Model):
-    CATEGORY_CHOICES = [
-        ('Dimsum', 'Dimsum'),
-        ('Chicken', 'Chicken'),
-        ('Pastry', 'Pastry'),
-        ('Sandwich', 'Sandwich'),
-        ('Dessert', 'Dessert'),
-        ('Drinks', 'Drinks'),
-    ]
-    category = models.CharField(max_length=100, choices=CATEGORY_CHOICES, default='Dimsum')
+    class Category(models.TextChoices):
+        NOODLES = 'Noodles', _('Noodles')
+        SANDWICH = 'Sandwich', _('Sandwich')
+        CHICKEN = 'Chicken', _('Chicken')
+        DIMSUM = 'Dimsum', _('Dimsum')
+        PASTRY = 'Pastry', _('Pastry')
+        DESSERT = 'Dessert', _('Dessert')
+        DRINKS = 'Drinks', _('Drinks')
+
+    category = models.CharField(max_length=100, choices=Category.choices, default=Category.DIMSUM)
     itemname = models.CharField(max_length=100)
 
 class Order(models.Model):
